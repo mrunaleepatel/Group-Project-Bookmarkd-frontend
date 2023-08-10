4902b81b9172b94cbb64679d41798875969d0b4d
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-08-09 20:10:47 -0400

    fixed Index.js component

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,16 +1,28 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, Form } from "react-router-dom";
 
 function Index(props) {
-    const bookmarks = useLoaderData()
-
-    return bookmarks.map(bookmark =>
-        <div key={bookmark._id} className="bookmark">
-            <Link to={`/${bookmark._id}`}>
-                <h1>{bookmark.title}</h1>
-            </Link>
-            <a><h2>{bookmark.url}</h2></a>
-        </div>
-    )
+    const bookmarks = useLoaderData();
+    
+    return (
+        <div>
+            <h2>Create a Bookmark</h2>
+            <Form action="/create" method="post">
+                <input type="text" name="name" placeholder="book's title"/>
+                <input type="text" name="url" placeholder="book's url" />
+                <input type="submit" value="Create Bookmark"/>
+            </Form>
+            {bookmarks.map((bookmark, index) => {
+                return(
+                    <div key={bookmark._id} className="bookmark">
+                        <Link to={`/${bookmark._id}`}>
+                            <h1>{bookmark.title}</h1>
+                        </Link>
+                    <a><h2>{bookmark.url}</h2></a>
+                    </div>
+                );
+        })}
+    </div>
+    );
 }
 
 export default Index;
